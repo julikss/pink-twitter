375e689b7e83ec3f6657367f2db9692843d9e5f9
julikss <julie.lehenka@gmail.com>
2022-10-16 22:48:08 +0300

    Deploy app to GitHub Pages

---
+++ precache-manifest.fbdd831988bbcdcbf3a9b4a6223f971d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b66ceede0cb4aef1fb98",
+    "url": "/pink-twitter/static/css/main.1e2183a6.chunk.css"
+  },
+  {
+    "revision": "b66ceede0cb4aef1fb98",
+    "url": "/pink-twitter/static/js/main.b66ceede.chunk.js"
+  },
+  {
+    "revision": "d2abe163afe9569d8c3b",
+    "url": "/pink-twitter/static/js/1.d2abe163.chunk.js"
+  },
+  {
+    "revision": "b81c08e9a0208a6a6917",
+    "url": "/pink-twitter/static/js/runtime~main.b81c08e9.js"
+  },
+  {
+    "revision": "95a45e7ac09348b3a06d406f40415e92",
+    "url": "/pink-twitter/static/media/pink-banner.95a45e7a.jpg"
+  },
+  {
+    "revision": "b25389bdb31d07db6aaca35cb640e346",
+    "url": "/pink-twitter/index.html"
+  }
+];
