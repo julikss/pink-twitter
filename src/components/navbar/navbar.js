1941769db677135da7a4d868bb4f5112032611c8
julikss <julie.lehenka@gmail.com>
2022-10-16 23:04:26 +0300

    Edit navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -8,7 +8,6 @@ const NavHeader = () => {
     return (
         <Navbar variant="light liner">
         <Container>
-          <img className="logo" src="../../../../../logo.png" alt="logo" />
           <Nav className="heading">Place for your thoughts</Nav>
           <Nav className="me-auto links">
             <Nav.Link href="https://github.com/julikss/pink-twitter">
